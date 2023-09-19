b610dfae2366e1b5a552e7f64a4187927fd15e0a
DESKTOP-OFF4UI3\The One <paul.ashbyvagg@gmail.com>
2023-09-19 18:29:18 +0200

    Vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 # Create your views here.
@@ -37,3 +38,39 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        # store the data and create vendor
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            # Creating the Vendor user
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            passsword = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=passsword)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            print('Restaurant is created from create_vendor')
+            messages.success(request, 'Your Restaurant account has been created and will be approved in 48hrs ')
+            return redirect('registerVendor')
+        else:
+            print('Invalid form')
+            print(form.errors)
+
+    form = UserForm()
+    v_form = VendorForm()
+
+    context = {
+        'form':form,
+        'v_form':v_form
+    }
+    return render(request, 'accounts/registerVendor.html', context)
---
+++ vendor/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from vendor.models import Vendor
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_verified', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+admin.site.register(Vendor, VendorAdmin)
