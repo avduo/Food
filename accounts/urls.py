e3ac6d1b8e524dc114db2bdb75b41f985ac0f60c
DESKTOP-OFF4UI3\The One <paul.ashbyvagg@gmail.com>
2023-09-19 23:08:47 +0200

    Customer and Vendor rigistration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registerUser/', views.registerUser, name='registerUser'),
-    path('registerVendor/', views.registerVendor, name='registerVendor'),
+    path('register-user/', views.registerUser, name='registerUser'),
+    path('register-vendor/', views.registerVendor, name='registerVendor'),
+
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('my-account/', views.myAccount, name='myAccount'),
+    path('customer-dashboard/', views.customerDashboard, name='customerDashboard'),
+    path('vendor-dashboard/', views.vendorDashboard, name='vendorDashboard'),
 ]
